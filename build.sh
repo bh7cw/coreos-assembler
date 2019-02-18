bh7cw/coreos-assembler

commit 1e2db9443e064573efb7902204081e629156e4d2

add depcheck() to verify RPMs installed
When testing builds of the container image using different distros
(Fedora/EL7), it is possible for the image build to complete
successfully, only to find out that the `preflight()` check in
`src/cmdlib.sh` bails out because an RPM was not installed.

This change introduces `depcheck()` as a way to verify that the
requested RPMs were installed.  The new function is used in the
`build.sh` script and as part of `preflight()` in `src/cmdlib.sh`.

build.sh
@@ -73,6 +73,10 @@ install_rpms() {
 
     # Further cleanup
     yum clean all
+
+    # shellcheck source=src/cmdlib.sh
+    . "${srcdir}/cmdlib.sh"
+    depcheck "${deps}"
 }
 
 _prep_make_and_make_install() {
@@ -126,7 +130,7 @@ configure_user(){
 
 write_archive_info() {
     # shellcheck source=src/cmdlib.sh
-    . "${srcdir}/src/cmdlib.sh"
+    . "${srcdir}/cmdlib.sh"
     mkdir -p /cosa /lib/coreos-assembler
     touch -f /lib/coreos-assembler/.clean
     prepare_git_artifacts /root/containerbuild /cosa/coreos-assembler-git.tar.gz /cosa/coreos-assembler-git.json

src/cmdlib.sh
@@ -84,12 +84,14 @@ has_privileges() {
     [ ${_privileged} == 1 ]
 }
 
-preflight() {
-    # Verify we have all dependencies
+depcheck() {
+    # Verify we have all dependencies installed
+    if [ -z "$1" ]; then
+      fatal "Must provide a list of RPMs to verify"
+    fi
+
     local deps
-    [ -n "${ISFEDORA}" ] && filter='^#FEDORA '
-    [ -n "${ISEL}" ]     && filter='^#EL7 '
-    deps=$(sed "s/${filter}//" /usr/lib/coreos-assembler/deps.txt | grep -v '^#')
+    deps=$1
     # Explicitly check the packages in one rpm -q to avoid
     # overhead, only drop down to individual calls if that fails.
     # We use --whatprovides so we handle file paths too.
@@ -105,6 +107,16 @@ preflight() {
         done
         fatal "Failed to find expected dependencies: $missing"
     fi
+}
+
+preflight() {
+    # Verify we have all dependencies
+    local deps
+    [ -n "${ISFEDORA}" ] && filter='^#FEDORA '
+    [ -n "${ISEL}" ]     && filter='^#EL7 '
+    deps=$(sed "s/${filter}//" /usr/lib/coreos-assembler/deps.txt | grep -v '^#')
+
+    depcheck "${deps}"
 
     if [ "$(stat -f --printf="%T" .)" = "overlayfs" ]; then
         fatal "$(pwd) must be a volume"

commit c6a12c9addfa0467ce86af2dbd0a6b60e4cac2b6

build: fix sourcing of cmdlib.sh write_archive_info
This fixes a regression in 1e2db9443e064573efb7902204081e629156e4d2

build.sh
@@ -130,7 +130,7 @@ configure_user(){
 
 write_archive_info() {
     # shellcheck source=src/cmdlib.sh
-    . "${srcdir}/cmdlib.sh"
+    . "${srcdir}/src/cmdlib.sh"
     mkdir -p /cosa /lib/coreos-assembler
     touch -f /lib/coreos-assembler/.clean
     prepare_git_artifacts /root/containerbuild /cosa/coreos-assembler-git.tar.gz /cosa/coreos-assembler-git.json