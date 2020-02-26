bh7cw/coreos-assembler

commit 73831285bf5f85af95bb202f0349545f45ac278f

qemu: Always add serial= for disks
This is used for device enumeration order, and is required for NVMe
drives apparently.  Hit this while trying to use a NVMe device as
a target for coreos-install testing (not primary disk).

platform/qemu.go
@@ -403,6 +403,9 @@ func (builder *QemuBuilder) addDiskImpl(disk *Disk, primary bool) error {
 	diskOpts := disk.DeviceOpts
 	if primary {
 		diskOpts = append(diskOpts, "serial=primary-disk")
+	} else {
+		// Note that diskId is incremented by addQcow2DiskFd
+		diskOpts = append(diskOpts, "serial="+fmt.Sprintf("disk%d", builder.diskId))
 	}
 	channel := disk.Channel
 	if channel == "" {

commit 356bde9b81b030407a4778bf535a2b0fca95eada

qemu: Only set serial= if not already provided
Regression from 73831285bf5f85af95bb202f0349545f45ac278f which
broke `coreos.ignition.mount.disks` because we were overriding
the provided serial.

platform/qemu.go
@@ -404,8 +404,16 @@ func (builder *QemuBuilder) addDiskImpl(disk *Disk, primary bool) error {
 	if primary {
 		diskOpts = append(diskOpts, "serial=primary-disk")
 	} else {
-		// Note that diskId is incremented by addQcow2DiskFd
-		diskOpts = append(diskOpts, "serial="+fmt.Sprintf("disk%d", builder.diskId))
+		foundserial := false
+		for _, opt := range diskOpts {
+			if strings.HasPrefix(opt, "serial=") {
+				foundserial = true
+			}
+		}
+		if !foundserial {
+			// Note that diskId is incremented by addQcow2DiskFd
+			diskOpts = append(diskOpts, "serial="+fmt.Sprintf("disk%d", builder.diskId))
+		}
 	}
 	channel := disk.Channel
 	if channel == "" {