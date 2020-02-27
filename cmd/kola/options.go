bh7cw/coreos-assembler

commit d4b252bb7c8b77e73ad6926de70921e95eeb95e5

kola: Rework syncOptions
Prep for adding more commands which need `syncOptions`; rather
than having `syncOptions` and `syncCosaOptions`, make the upgrade
test the exception that skips the latter.

cmd/kola/options.go
@@ -142,7 +142,7 @@ func init() {
 }
 
 // Sync up the command line options if there is dependency
-func syncOptions() error {
+func syncOptionsImpl(useCosa bool) error {
 	kola.PacketOptions.Board = kola.QEMUOptions.Board
 	kola.PacketOptions.GSOptions = &kola.GCEOptions
 
@@ -169,10 +169,8 @@ func syncOptions() error {
 		return err
 	}
 
-	if kola.Options.CosaBuild != "" {
-		var err error
-		kola.CosaBuild, err = cosa.ParseBuild(kola.Options.CosaBuild)
-		if err != nil {
+	if useCosa && kola.Options.CosaBuild != "" {
+		if err := syncCosaOptions(); err != nil {
 			return err
 		}
 	}
@@ -201,11 +199,18 @@ func syncOptions() error {
 	return nil
 }
 
+// syncOptions updates default values of options based on provided ones
+func syncOptions() error {
+	return syncOptionsImpl(true)
+}
+
 // syncCosaOptions parses the cosa build and sets unset platform-specific
 // options that can be derived from the cosa build metadata
 func syncCosaOptions() error {
-	if kola.CosaBuild == nil {
-		return nil
+	var err error
+	kola.CosaBuild, err = cosa.ParseBuild(kola.Options.CosaBuild)
+	if err != nil {
+		return err
 	}
 
 	switch kolaPlatform {

cmd/kola/kola.go
@@ -117,9 +117,6 @@ func main() {
 
 func preRun(cmd *cobra.Command, args []string) {
 	err := syncOptions()
-	if err == nil {
-		err = syncCosaOptions()
-	}
 
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
@@ -390,7 +387,7 @@ func preRunUpgrade(cmd *cobra.Command, args []string) error {
 		errors.New("Error: missing required argument --cosa-build")
 	}
 
-	err := syncOptions()
+	err := syncOptionsImpl(false)
 	if err != nil {
 		return err
 	}

commit e1d24b3e893f61c639dee3c114912efc2225aebc

kola: Fix syncOptions
Regression from d4b252bb7c8b77e73ad6926de70921e95eeb95e5
We need to keep parsing the cosa build, just not actually use it
to sync the target options in order to match how the upgrade
test works.

cmd/kola/options.go
@@ -143,6 +143,8 @@ func init() {
 
 // Sync up the command line options if there is dependency
 func syncOptionsImpl(useCosa bool) error {
+	var err error
+
 	kola.PacketOptions.Board = kola.QEMUOptions.Board
 	kola.PacketOptions.GSOptions = &kola.GCEOptions
 
@@ -169,9 +171,15 @@ func syncOptionsImpl(useCosa bool) error {
 		return err
 	}
 
-	if useCosa && kola.Options.CosaBuild != "" {
-		if err := syncCosaOptions(); err != nil {
+	if kola.Options.CosaBuild != "" {
+		kola.CosaBuild, err = cosa.ParseBuild(kola.Options.CosaBuild)
+		if err != nil {
 			return err
+		}
+		if useCosa {
+			if err := syncCosaOptions(); err != nil {
+				return err
+			}
 		}
 	}
 
@@ -204,15 +212,9 @@ func syncOptions() error {
 	return syncOptionsImpl(true)
 }
 
-// syncCosaOptions parses the cosa build and sets unset platform-specific
+// syncCosaOptions sets unset platform-specific
 // options that can be derived from the cosa build metadata
 func syncCosaOptions() error {
-	var err error
-	kola.CosaBuild, err = cosa.ParseBuild(kola.Options.CosaBuild)
-	if err != nil {
-		return err
-	}
-
 	switch kolaPlatform {
 	case "qemu-unpriv", "qemu":
 		if kola.QEMUOptions.DiskImage == "" {